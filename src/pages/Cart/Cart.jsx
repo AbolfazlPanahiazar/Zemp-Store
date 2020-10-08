52344470e667e13d9ac70b9fe476f1859c66f007
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2020-10-08 14:10:43 +0330

    Done with cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,16 +1,52 @@
 import React from "react";
 import { connect, useSelector } from "react-redux";
-import { Box, Typography, Grid, IconButton } from "@material-ui/core";
+import { Box, Typography, Grid, IconButton, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
-import { ShoppingCart, Add, Remove } from "@material-ui/icons";
+import { Add, Remove, Delete, ClearAll } from "@material-ui/icons";
+import { toast } from "react-toastify";
 
 import { increaseNumbers, decreaseNumbers, removeFromCart, clearCart } from "../../store/cart/cartActions";
 
 const useStyles = makeStyles(() => ({
   root: {},
-  gridItem: {},
+  gridItem: {
+    borderTop: "1px solid #ccc",
+  },
+  heading: {
+    fontSize: "1.2rem",
+    fontWeight: "bold",
+    marginBottom: "10px",
+  },
   image: {
     maxWidth: "100%",
+  },
+  countButtons: {
+    width: "40px",
+    height: "40px",
+    backgroundColor: "#F0F0F5",
+    color: "#6E70FF",
+  },
+  trashButton: {
+    width: "40px",
+    height: "40px",
+    backgroundColor: "#F0F0F5",
+    color: "#DD0000",
+  },
+  clearButton: {
+    border: "1px solid #ccc",
+    color: "#888",
+    width: "50px",
+    height: "50px",
+  },
+  submitButton: {
+    backgroundColor: "#6E70FF",
+    color: "#FFFFFF",
+    width: "200px",
+    marginLeft: "10px",
+    height: "50px",
+    "&:hover": {
+      color: "#000000",
+    },
   },
 }));
 
@@ -19,27 +55,54 @@ function Cart({ increaseNumbers, decreaseNumbers, removeFromCart, clearCart }) {
   const classes = useStyles();
 
   return (
-    <Box width="100%" bgcolor="red">
-      <Box>
-        <Typography>سبد خرید</Typography>
+    <Box width="100%" display="flex" flexDirection="column" alignItems="center">
+      <Box padding="10px">
+        <Typography className={classes.heading}>سبد خرید</Typography>
       </Box>
-      <Box>
-        <Grid xs={1} sm={2} container>
-          {cart.products.map((item) => (
-            <Grid key={item.id} item>
-              <Box width="100%" display="flex">
-                <Box maxWidth="150px">
-                  <img className={classes.image} src={item.images[0].src} title={item.name} />
+      <Box width="97%" display="flex" justifyContent="center">
+        <Grid container>
+          {cart.products.length === 0 ? (
+            <Typography className={classes.heading}>هیچ محصولی در سبد خرید موجود نیست</Typography>
+          ) : (
+            cart.products.map((item) => (
+              <Grid className={classes.gridItem} key={item.id} item xs={12} sm={6}>
+                <Box width="100%" minHeight="100px" display="flex" alignItems="center" justifyContent="space-between" paddingX="5px">
+                  <Box maxWidth="150px">
+                    <img className={classes.image} src={item.images[0].src} title={item.name} />
+                  </Box>
+                  <Box padding="5px">
+                    <Typography>{item.name}</Typography>
+                    <Typography>{item.price}</Typography>
+                  </Box>
+                  <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                    <IconButton onClick={() => increaseNumbers(item.id)} className={classes.countButtons}>
+                      <Add />
+                    </IconButton>
+                    <Typography>{item.count}</Typography>
+                    {item.count > 1 && (
+                      <IconButton onClick={() => decreaseNumbers(item.id)} className={classes.countButtons}>
+                        <Remove />
+                      </IconButton>
+                    )}
+                  </Box>
+                  <Box>
+                    <IconButton onClick={() => removeFromCart(item.id)} className={classes.trashButton}>
+                      <Delete />
+                    </IconButton>
+                  </Box>
                 </Box>
-                <Box>
-                  <Typography>{item.name}</Typography>
-                  <Typography>{item.price}</Typography>
-                  <Typography>{item.count}</Typography>
-                </Box>
-              </Box>
-            </Grid>
-          ))}
+              </Grid>
+            ))
+          )}
         </Grid>
+      </Box>
+      <Box dispaly="flex" marginY="10px">
+        <Button onClick={() => toast.success("از اعتماد شما متشکریم")} className={classes.submitButton}>
+          تکمیل خرید
+        </Button>
+        <Button onClick={() => clearCart()} className={classes.clearButton}>
+          <ClearAll />
+        </Button>
       </Box>
     </Box>
   );
--- src/store/cart/cartActions.js
+++ src/store/cart/cartActions.js
@@ -1,8 +1,12 @@
 import types from "./cartActionTypes";
+import { toast } from "react-toastify";
 
 export const addToCart = (product, count) => ({ type: types.ADD, payload: { product: product, count: count } });
 
-export const removeFromCart = (productId) => ({ type: types.REMOVE, payload: productId });
+export const removeFromCart = (productId) => {
+  toast.error("محصول از سبد خرید حذف شد");
+  return { type: types.REMOVE, payload: productId };
+};
 
 export const increaseNumbers = (productId) => ({ type: types.INCREASE, payload: productId });
 
