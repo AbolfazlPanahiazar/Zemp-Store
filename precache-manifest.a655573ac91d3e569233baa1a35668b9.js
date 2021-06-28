0e90046270f7992e32bc2e45b683cffb861e20a1
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2021-06-28 09:34:22 +0430

    Updates

---
+++ precache-manifest.a655573ac91d3e569233baa1a35668b9.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ca8aa461ae0458f57e84ee52ec3b14",
+    "url": "/Zemp-Store/index.html"
+  },
+  {
+    "revision": "541eb783b353d311d314",
+    "url": "/Zemp-Store/static/css/3.d34346ea.chunk.css"
+  },
+  {
+    "revision": "b1319b3b98da53019790",
+    "url": "/Zemp-Store/static/css/4.cecd102b.chunk.css"
+  },
+  {
+    "revision": "1258908ab444607ec59e",
+    "url": "/Zemp-Store/static/css/5.4a005902.chunk.css"
+  },
+  {
+    "revision": "d553c2e549f02ec4e6e3",
+    "url": "/Zemp-Store/static/css/main.f99ff798.chunk.css"
+  },
+  {
+    "revision": "7e54b6796ca78466c01e",
+    "url": "/Zemp-Store/static/js/0.7d943a94.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/Zemp-Store/static/js/0.7d943a94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "541eb783b353d311d314",
+    "url": "/Zemp-Store/static/js/3.11fb1b72.chunk.js"
+  },
+  {
+    "revision": "872d4ad223c39fec1068d862c0b329ff",
+    "url": "/Zemp-Store/static/js/3.11fb1b72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1319b3b98da53019790",
+    "url": "/Zemp-Store/static/js/4.4986f458.chunk.js"
+  },
+  {
+    "revision": "1258908ab444607ec59e",
+    "url": "/Zemp-Store/static/js/5.c893eeb9.chunk.js"
+  },
+  {
+    "revision": "ca8beb0925f98e155b5c",
+    "url": "/Zemp-Store/static/js/6.ebc73fb6.chunk.js"
+  },
+  {
+    "revision": "52337267b1d791c0ff04",
+    "url": "/Zemp-Store/static/js/7.f89e7db0.chunk.js"
+  },
+  {
+    "revision": "d553c2e549f02ec4e6e3",
+    "url": "/Zemp-Store/static/js/main.6f9fcf7b.chunk.js"
+  },
+  {
+    "revision": "e477619f30668871cfba",
+    "url": "/Zemp-Store/static/js/runtime-main.006963f7.js"
+  },
+  {
+    "revision": "abdaaf2e791f4416323efffec0a85b49",
+    "url": "/Zemp-Store/static/media/Vazir.abdaaf2e.woff"
+  },
+  {
+    "revision": "fd58729f3709a9e093f9692c9e4914bf",
+    "url": "/Zemp-Store/static/media/Vazir.fd58729f.eot"
+  },
+  {
+    "revision": "6c12908b151988a7f6e47c655680abb8",
+    "url": "/Zemp-Store/static/media/banner1.6c12908b.gif"
+  },
+  {
+    "revision": "1acb675c78f508be992aa7d031d006a1",
+    "url": "/Zemp-Store/static/media/banner2.1acb675c.jpg"
+  },
+  {
+    "revision": "d365d8f63589d5932fdfff37a198172a",
+    "url": "/Zemp-Store/static/media/banner21.d365d8f6.jpg"
+  },
+  {
+    "revision": "5555dd4c4b5ff00a5c29258b8ce5b7df",
+    "url": "/Zemp-Store/static/media/banner22.5555dd4c.jpg"
+  },
+  {
+    "revision": "854480e63716ab629cb6619829437cbe",
+    "url": "/Zemp-Store/static/media/banner23.854480e6.jpg"
+  },
+  {
+    "revision": "4345895e013d9bfa9e2cec1158882981",
+    "url": "/Zemp-Store/static/media/banner24.4345895e.jpg"
+  },
+  {
+    "revision": "7f9bedf6853928b0be0e3e0074c52c5c",
+    "url": "/Zemp-Store/static/media/slide11.7f9bedf6.jpg"
+  },
+  {
+    "revision": "c10dbfa8722ce2e36881f00ba3091c97",
+    "url": "/Zemp-Store/static/media/slide12.c10dbfa8.jpg"
+  },
+  {
+    "revision": "79837978d333408f59cad174ecac6504",
+    "url": "/Zemp-Store/static/media/slide21.79837978.jpg"
+  },
+  {
+    "revision": "ab860c4074d1b408e04664688ce69096",
+    "url": "/Zemp-Store/static/media/slide22.ab860c40.jpg"
+  },
+  {
+    "revision": "7d86a707871217a6663a23c01946ed4c",
+    "url": "/Zemp-Store/static/media/slide23.7d86a707.jpg"
+  }
+]);
